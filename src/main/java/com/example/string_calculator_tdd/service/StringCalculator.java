c23bca5cba2408fda7f0edd71afb448286c40b23
harsh-preet76 <harshpreets331@gmail.com>
2025-09-29 12:47:55 +0530

    feat/refactor: Enhance add() to support custom delimiters numbers (green)

--- src/main/java/com/example/string_calculator_tdd/service/StringCalculator.java
+++ src/main/java/com/example/string_calculator_tdd/service/StringCalculator.java
@@ -3,17 +3,33 @@ package com.example.string_calculator_tdd.service;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		
-		if(numbers == null || numbers.isEmpty()) {
+
+		if (numbers == null || numbers.isEmpty()) {
 			return 0;
 		}
-		
-		String[] parts = numbers.split(",");
-		int sum = 0;
-		for (String part: parts) {
-			sum += Integer.parseInt(part);
+
+		String delimiter = ",";
+		String input = numbers;
+
+		if (numbers.startsWith("//")) {
+			int delimitedEndIndex = numbers.indexOf("\n");
+			System.out.println("Delimiter End: " + delimitedEndIndex);
+			delimiter = numbers.substring(2, delimitedEndIndex);
+			System.out.println("Delimeter: " + delimiter);  
+			input = numbers.substring(delimitedEndIndex + 1);
+			System.out.println("Input: " + input);
 		}
-		
+
+		String[] parts = input.split("[\n" + delimiter + "]");
+		int sum = 0;
+		for (String part : parts) {
+			System.out.println("Part Values: " + part);
+			if (!part.isEmpty()) {
+				sum += Integer.parseInt(part);
+			}
+		}
+		System.out.println("Sum Values: " + sum);
 		return sum;
 	}
+
 }
