f43b0cdb237724c8a9d8217d6fdc1592140c10df
harsh-preet76 <harshpreets331@gmail.com>
2025-09-29 11:12:27 +0530

    feat/refactor: Enhance add() to support sum of two comma-separated numbers (green)

--- src/main/java/com/example/string_calculator_tdd/service/StringCalculator.java
+++ src/main/java/com/example/string_calculator_tdd/service/StringCalculator.java
@@ -3,9 +3,17 @@ package com.example.string_calculator_tdd.service;
 public class StringCalculator {
 
 	public int add(String numbers) {
+		
 		if(numbers == null || numbers.isEmpty()) {
 			return 0;
 		}
-		return Integer.parseInt(numbers);
+		
+		String[] parts = numbers.split(",");
+		int sum = 0;
+		for (String part: parts) {
+			sum += Integer.parseInt(part);
+		}
+		
+		return sum;
 	}
 }
